71f2462a9acb56a8b4c7eee6403ea536d29d8ec6
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2004-12-11 00:17:28 +0000

    LPGLed\!
    git-svn-id: file:///var/svn/lua-gd/trunk@36 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- luagd.c
+++ luagd.c
@@ -1,8 +1,23 @@
 /*
- * luagd (c) 2004 Alexandre Erwin Ittner <aittner@netuno.com.br>
- * GD bindings to Lua. Like Lua, this program is released under
- * the MIT license (see http://www.lua.org/copyright.html).
- * WITHOUT ANY WARRANTY.
+ * luagd -- GD bindings to Lua.
+ * (c) 2004 Alexandre Erwin Ittner <aittner@netuno.com.br>
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
  *
  * $Id$
  *
