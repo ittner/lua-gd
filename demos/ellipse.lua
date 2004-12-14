b9584b2de5f9420e4fab5c3cc459cf8265c29481
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2004-12-14 23:43:02 +0000

    *** empty log message ***
    git-svn-id: file:///var/svn/lua-gd/trunk@60 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

---
+++ demos/ellipse.lua
@@ -0,0 +1,12 @@
+load_gd = assert(loadlib("./libluagd.so", "luaopen_gd"))
+load_gd()
+
+im = gd.createTrueColor(80, 80)
+assert(im)
+
+black = im:colorAllocate(0, 0, 0)
+white = im:colorAllocate(255, 255, 255)
+im:filledEllipse(40, 40, 70, 50, white)
+
+im:png("./out.png")
+os.execute("display out.png")
