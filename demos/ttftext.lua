f3847ddb304b4eaec24f23081733154126f3b098
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2004-12-23 14:52:18 +0000

    *** empty log message ***
    git-svn-id: file:///var/svn/lua-gd/trunk@63 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- demos/ttftext.lua
+++ demos/ttftext.lua
@@ -2,9 +2,10 @@ load_gd = assert(loadlib("libluagd.so", "luaopen_gd"))
 load_gd()
 
 
-function boxedtext(im, color, size, ang, x, y, text, bcolor)
+function boxedtext(im, color, font, size, ang, x, y, text, bcolor)
+  font = "./" .. font .. ".ttf"
   llx, lly, lrx, lry, urx, ury, ulx, uly =
-    im:stringFT(color, "./Vera.ttf", size, math.rad(ang), x, y, text)
+    im:stringFT(color, font, size, math.rad(ang), x, y, text)
   im:polygon({ {llx, lly}, {lrx, lry}, {urx, ury}, {ulx, uly} }, bcolor)
 end
 
@@ -19,11 +20,11 @@ greent = im:colorAllocateAlpha(0, 250, 0, 70)
 lbluet = im:colorAllocateAlpha(180, 180, 255, 70)
 yellowt = im:colorAllocateAlpha(240, 240, 0, 70)
 
-boxedtext(im, yellowt, 300, 0, 60, 350, "A", bluet)
-boxedtext(im, greent, 80, 45, 60, 220, "Ithil", bluet)
-boxedtext(im, redt, 45, 90, 380, 300, "Lua-GD", bluet)
-boxedtext(im, lbluet, 36, 290, 160, 130, "FreeType", bluet)
-boxedtext(im, grayt, 26, 180, 390, 360, "Turn 180° before read", bluet)
+boxedtext(im, yellowt, "Vera", 300, 0, 60, 350, "A", bluet)
+boxedtext(im, greent, "Vera", 80, 45, 60, 220, "Ithil", bluet)
+boxedtext(im, redt, "Vera", 45, 90, 380, 300, "Lua-GD", bluet)
+boxedtext(im, lbluet, "Vera", 36, 290, 160, 130, "FreeType", bluet)
+boxedtext(im, grayt, "Vera", 26, 180, 390, 360, "Turn 180° before read", bluet)
 
 im:png("./out.png")
 os.execute("display out.png")
