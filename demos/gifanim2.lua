8fcadd41b752388929d4375a629fc516167feb33
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2005-01-03 19:55:55 +0000

    *** empty log message ***
    git-svn-id: file:///var/svn/lua-gd/trunk@121 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

---
+++ demos/gifanim2.lua
@@ -0,0 +1,23 @@
+load_gd = assert(loadlib("libluagd.so", "luaopen_gd"))
+load_gd()
+
+im = gd.createPalette(120, 120)
+assert(im)
+
+black = im:colorAllocate(0, 0, 0)
+blue = {}
+for i = 1, 20 do
+  blue[i] = im:colorAllocate(0, 0, 12 * i)
+end
+
+im:gifAnimBegin("out.gif", true, 0)
+for i = 1, 20 do
+  tim = gd.createPalette(120, 120)
+  tim:paletteCopy(im)
+  tim:arc(60, 60, 6*i, 6*i, 0, 360, blue[20-1])
+  tim:gifAnimAdd("out.gif", false, 0, 0, 5, gd.DISPOSAL_NONE)
+end
+
+gd.gifAnimEnd("out.gif")
+
+print("Gif animation written to file 'out.gif'.")
