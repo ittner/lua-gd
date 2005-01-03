76075a01b19526267178ae6ff391798b73f4c568
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2005-01-03 20:01:41 +0000

    *** empty log message ***
    git-svn-id: file:///var/svn/lua-gd/trunk@122 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- demos/gifanim2.lua
+++ demos/gifanim2.lua
@@ -7,14 +7,14 @@ assert(im)
 black = im:colorAllocate(0, 0, 0)
 blue = {}
 for i = 1, 20 do
-  blue[i] = im:colorAllocate(0, 0, 12 * i)
+  blue[i] = im:colorAllocate(0, 0, 120 + 6 * i)
 end
 
 im:gifAnimBegin("out.gif", true, 0)
 for i = 1, 20 do
   tim = gd.createPalette(120, 120)
   tim:paletteCopy(im)
-  tim:arc(60, 60, 6*i, 6*i, 0, 360, blue[20-1])
+  tim:arc(60, 60, 6*i, 6*i, 0, 360, blue[21-i])
   tim:gifAnimAdd("out.gif", false, 0, 0, 5, gd.DISPOSAL_NONE)
 end
 
