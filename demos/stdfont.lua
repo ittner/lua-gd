523b818148516cb2b88ee2e2a3c037cbc2bc0f54
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2005-03-19 17:20:17 +0000

    Example changed for use in the docs
    git-svn-id: file:///var/svn/lua-gd/trunk@174 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- demos/stdfont.lua
+++ demos/stdfont.lua
@@ -1,16 +1,17 @@
 require "gd"
 
-im = gd.createTrueColor(80, 110)
-assert(im)
+x, y = 140, 110
 
-black = im:colorAllocate(0, 0, 0)
+im = assert(gd.createTrueColor(x, y))
 white = im:colorAllocate(255, 255, 255)
+black = im:colorAllocate(0, 0, 0)
+im:filledRectangle(0, 0, x, y, white)
 
-im:string(gd.FONT_TINY, 20, 20, "Tiny", white)
-im:string(gd.FONT_SMALL, 20, 30, "Small", white)
-im:string(gd.FONT_MEDIUM, 20, 45, "Medium", white)
-im:string(gd.FONT_LARGE, 20, 58, "Large", white)
-im:string(gd.FONT_GIANT, 20, 75, "Giant", white)
+im:string(gd.FONT_TINY, 10, 20, "gd.FONT_TINY", black)
+im:string(gd.FONT_SMALL, 10, 30, "gd.FONT_SMALL", black)
+im:string(gd.FONT_MEDIUM, 10, 45, "gd.FONT_MEDIUM", black)
+im:string(gd.FONT_LARGE, 10, 58, "gd.FONT_LARGE", black)
+im:string(gd.FONT_GIANT, 10, 75, "gd.FONT_GIANT", black)
 
 im:png("out.png")
 os.execute("display out.png")
