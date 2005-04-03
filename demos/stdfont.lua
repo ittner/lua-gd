fe2d6b4a23c1fb5a621e386c63a11767c71db375
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2005-04-03 16:07:34 +0000

    *** empty log message ***
    git-svn-id: file:///var/svn/lua-gd/trunk@198 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- demos/stdfont.lua
+++ demos/stdfont.lua
@@ -2,16 +2,18 @@ require "gd"
 
 x, y = 140, 110
 
-im = assert(gd.createTrueColor(x, y))
+im = gd.createPalette(x, y)
 white = im:colorAllocate(255, 255, 255)
 black = im:colorAllocate(0, 0, 0)
-im:filledRectangle(0, 0, x, y, white)
 
-im:string(gd.FONT_TINY, 10, 20, "gd.FONT_TINY", black)
-im:string(gd.FONT_SMALL, 10, 30, "gd.FONT_SMALL", black)
-im:string(gd.FONT_MEDIUM, 10, 45, "gd.FONT_MEDIUM", black)
-im:string(gd.FONT_LARGE, 10, 58, "gd.FONT_LARGE", black)
-im:string(gd.FONT_GIANT, 10, 75, "gd.FONT_GIANT", black)
+im:string(gd.FONT_TINY, 10, 10, "gd.FONT_TINY", black)
+im:string(gd.FONT_SMALL, 10, 20, "gd.FONT_SMALL", black)
+im:string(gd.FONT_MEDIUM, 10, 35, "gd.FONT_MEDIUM", black)
+im:string(gd.FONT_LARGE, 10, 48, "gd.FONT_LARGE", black)
+im:string(gd.FONT_GIANT, 10, 65, "gd.FONT_GIANT", black)
 
-im:png("out.png")
-os.execute("display out.png")
+im:line(60, 93, 70, 93, black)
+im:string(gd.FONT_SMALL, 80, 86, "= 10 px", black)
+
+im:png("stdfonts.png")
+
