543a4184b3c8c1960fd27bb640475480cfa5fa2f
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2005-07-25 21:11:03 +0000

    Installer
    git-svn-id: file:///var/svn/lua-gd/trunk@225 054e6bdc-8f27-0410-bf6a-9daf6eb585fa

--- install51.lua
+++ install51.lua
@@ -1,13 +1,12 @@
--- Easy way to put  a library on the path.
--- (c) 2005 Alexandre Erwin Ittner 
+#!/usr/bin/env lua
+-- Easy way to put a library on the path. For Lua 5.1 only.
+-- (c) 2005 Alexandre Erwin Ittner < aittner{at}netuno.com.br >
 --
---  print(string.gsub("aittnerXnetunoYcomYbr", "[XY]",
---     function(x) return ({ X = '@', Y = '.'})[x] end))
---
+-- This file is distributed under the same license that Lua 5.1 itself.
 --
 
 
--- Splits an package.cpath into a table.
+-- Splits a package.cpath into a table.
 function splitpath(str)
   local t = {}
   local v
@@ -16,6 +15,19 @@ function splitpath(str)
   end
   return t
 end
+
+
+-- Checks if a given file exists. DUMB. Do not distinguish files from
+-- symlinks, pipes, sockets, device drivers, etc.
+function fileexists(fname)
+  local fp = io.open(fname, "r")
+  if fp then
+    fp:close()
+    return true
+  end
+  return false
+end
+
 
 -- Finds the first absolute path on the list. Works with *nix (MacOS X
 -- included) and Windows paths.
@@ -29,13 +41,50 @@ function findplace(tbl)
   end
 end
 
-function copy(fname, name)
+
+-- It's REALY stupid, but also it's REALY cross-platform
+function dumbcopy(from, to)
+  local bufsize = 512*1024  -- 512K at once
+  local ifp = io.open(from, "rb")
+  assert(ifp, "Can't open file for reading. File not found?")
+  local ofp = io.open(to, "wb")
+  assert(ofp, "Can't open file for writing. Permission denied?")
+  local buf
+  repeat
+    buf = ifp:read(bufsize)
+    if buf then
+      ofp:write(buf)
+    end
+  until not buf
+  ifp:close()
+  ofp:close()
+end
+
+
+-- Copies the file. 'to' MUST be an absolute path!
+function copy(from, to)
+  if to:sub(1, 1) == "/" then       -- Cool! We are on Unix, guys!
+    ret = os.execute('install -D "' .. from .. '" "' .. to .. '"')
+    if ret == 0 and fileexists(to) then
+      return
+    end
+  end
+  -- Let's try some violence.
+  dumbcopy(from, to)
+end
+
+
+-- Installs one file. Simple. Perfect.
+function installfile(fname, name)
   local tpath = splitpath(package.cpath)
   local path = findplace(tpath)
   assert(path, "Can't find a place to install")
   local dest = path:gsub("%?", name)
-  print(dest)
+  print("Install:", dest)
+  copy(fname, dest)
 end
 
 
-copy(arg[1], arg[2])
+assert(arg[1] and arg[2], "No arguments provided")
+installfile(arg[1], arg[2])
+os.exit(0)
